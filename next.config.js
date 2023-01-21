3a7ee3f76dc2c1d5aeab1bf510b102c65ed3ca50
dmytro <dmytro@ofload.com>
2023-01-21 21:40:36 +0100

    added domain for the images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {},
+  images: {
+    domains: [
+      "uppr.com.ua",
+    ],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config = {
