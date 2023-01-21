897300da79cfe6413e6787f7c83a1555d5314e57
dmytro <dmytro@ofload.com>
2023-01-21 23:53:11 +0100

    test with local images

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,6 @@ import PageNotFound from '../components/common/404/404';
 const DEFAULT_REDIRECTS_PARAMS = {redirectLink:'/blog', redirectPage:'Повернутись до блогу'};
 
 export default function Page404(props) {
-    console.log('====>', props);
     const {redirectLink, redirectPage } = !Object.keys(props).length ? DEFAULT_REDIRECTS_PARAMS : props;
 
     return (
