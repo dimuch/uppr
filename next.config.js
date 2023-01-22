1bb9c9362a925b092b67fa15209e7d8d1d617794
dmytro <dmytro@ofload.com>
2023-01-22 02:10:16 +0100

    test with local images

--- next.config.js
+++ next.config.js
@@ -4,26 +4,7 @@ const nextConfig = {
     reactStrictMode: true,
     experimental: {},
     images: {
-        // limit of 25 deviceSizes values
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        // limit of 25 imageSizes values
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        // limit of 50 domains values
-        domains: ['127.0.0.1:3000', 'uppr.com.ua'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'uppr.com.ua',
-                port: '',
-                pathname: '/public/**'
-            },
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '3000',
-                pathname: '/public/**'
-            }
-        ]
+        domains: ['127.0.0.1:3000', 'uppr.com.ua']
     },
     webpack: (config, {isServer}) => {
         if (!isServer) {
