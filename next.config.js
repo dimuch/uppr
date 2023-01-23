875222a58b3af6e11b2344b6f9ad692513d9908d
dmytro <dmytro@ofload.com>
2023-01-23 17:49:29 +0100

    Test Nextjs Image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 const nextConfig = {
     reactStrictMode: true,
@@ -21,29 +20,6 @@ const nextConfig = {
                         crypto: false,
                     },
                 },
-                optimization: {
-                    minimizer: [
-                        new ImageMinimizerPlugin({
-                            minimizer: {
-                                implementation: ImageMinimizerPlugin.sharpMinify,
-                            },
-                            generator: [
-                                {
-                                    // You can apply generator using `?as=webp`, you can use any name and provide more options
-                                    preset: "webp",
-                                    implementation: ImageMinimizerPlugin.sharpGenerate,
-                                    options: {
-                                        encodeOptions: {
-                                            webp: {
-                                                quality: 90,
-                                            },
-                                        },
-                                    },
-                                },
-                            ],
-                        }),
-                    ],
-                },
             };
         }
         return config;
@@ -58,9 +34,9 @@ const nextConfig = {
         // path prefix for Image Optimization API, useful with `loader`
         path: '/_next/image',
         // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
-        loader: 'default',
+        loader: 'custom',
         // file with `export default function loader({src, width, quality})`
-        // loaderFile: './components/common/loader/loader.js',
+        loaderFile: './components/common/loader/loader.js',
         // disable static imports for image files
         disableStaticImages: false,
         // minimumCacheTTL is in seconds, must be integer 0 or more
