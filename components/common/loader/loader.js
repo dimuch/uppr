f748026769dfc8e2d2d3b129966d8a721498f5dd
dmytro <dmytro@ofload.com>
2023-01-24 01:34:24 +0100

    Test Nextjs Image optimization

--- components/common/loader/loader.js
+++ components/common/loader/loader.js
@@ -1,4 +1,3 @@
 export default function loader({ src, width, quality }){
-    console.log('===> ', `${src}?w=${width}&q=${quality || 75}`)
     return `${src}?w=${width}&q=${quality || 75}`
 }
--- next.config.js
+++ next.config.js
@@ -1,9 +1,67 @@
 /** @type {import('next').NextConfig} */
 
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 const nextConfig = {
     reactStrictMode: true,
     experimental: {},
     webpack: (config, {isServer}) => {
+        const addOn = {
+            module: {
+                ...config.module,
+                rules: [].concat(config.module.rules,
+                    [
+                        {
+                            test: /\.(jpe?g|png|gif|svg)$/i,
+                            type: 'asset',
+                        },
+                        {
+                            test: /\.(jpe?g|png|gif|svg)$/i,
+                            use: [
+                                {
+                                    loader: ImageMinimizerPlugin.loader,
+                                    options: {
+                                        severityError: 'warning',
+                                        minimizerOptions: {
+                                            plugins: ['gifsicle'],
+                                        },
+                                    },
+                                },
+                            ],
+                        },
+                    ])
+            },
+            optimization: {
+                ...config.optimization,
+                minimizer: [].concat(config.optimization.minimizer,
+                    [
+                        new ImageMinimizerPlugin({
+                            minimizer: {
+                                implementation: ImageMinimizerPlugin.sharpMinify,
+                            },
+                            generator: [
+                                {
+                                    type: "asset",
+                                    implementation: ImageMinimizerPlugin.sharpGenerate,
+                                    options: {
+                                        encodeOptions: {
+                                            webp: {
+                                                quality: 90,
+                                            },
+                                        },
+                                    },
+                                },
+                            ],
+                        }),
+                    ]
+                )
+            },
+            plugins: [].concat(...config.plugins, [
+                new CopyPlugin({ patterns: ["./public/assets/images/**/*"] })
+            ])
+        };
+
         if (!isServer) {
             config = {
                 ...config,
@@ -20,63 +78,15 @@ const nextConfig = {
                         crypto: false,
                     },
                 },
-            };
+            }
+            return {...config, ...addOn};
         }
-        return config;
+
+        return {...config, ...addOn};
     },
-    images: {
-        // limit of 25 deviceSizes values
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-        // limit of 25 imageSizes values
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        // limit of 50 domains values
-        domains: ['127.0.0.1', 'localhost', 'uppr.com.ua'],
-        // path prefix for Image Optimization API, useful with `loader`
-        // path: '/public',
-        // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
-        loader: 'custom',
-        // file with `export default function loader({src, width, quality})`
-        loaderFile: './components/common/loader/loader.js',
-        // disable static imports for image files
-        disableStaticImages: false,
-        // minimumCacheTTL is in seconds, must be integer 0 or more
-        minimumCacheTTL: 60,
-        // ordered list of acceptable optimized image formats (mime types)
-        formats: ['image/webp'],
-        // enable dangerous use of SVG images
-        dangerouslyAllowSVG: false,
-        // set the Content-Security-Policy header
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-        // limit of 50 objects
-        // remotePatterns: [
-        //     {
-        //         protocol: "http",
-        //         hostname: " localhost",
-        //         port: '3000',
-        //         pathname: '/public/**',
-        //     },
-        //     {
-        //         protocol: "http",
-        //         hostname: "127.0.0.1",
-        //         port: '3000',
-        //         pathname: '/public/**',
-        //     },
-        //     {
-        //         protocol: "http",
-        //         hostname: "0.0.0.0",
-        //         port: '3000',
-        //         pathname: '/public/**',
-        //     },
-        //     {
-        //         protocol: "https",
-        //         hostname: "uppr.com.ua",
-        //         port: '',
-        //         pathname: '/public/**',
-        //     },
-        // ],
-        // when true, every image will be unoptimized
-        // unoptimized: false
-    },
+    images:{
+        unoptimized: false
+    }
 };
 
-module.exports = nextConfig;
+    module.exports = nextConfig;
