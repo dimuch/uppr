d236b6c7a61bd9b48cc4bf159f5af722931abd88
dmytro <dmytro@ofload.com>
2023-01-31 00:53:06 +0100

    Setting up work flow

--- next.config.js
+++ next.config.js
@@ -90,7 +90,7 @@ const nextConfig = {
         // limit of 25 imageSizes values
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         // path prefix for Image Optimization API, useful with `loader`
-        path: '/_next/image',
+        // path: '/_next/image',
         // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
         loader: 'custom',
         // file with `export default function loader({src, width, quality})`
