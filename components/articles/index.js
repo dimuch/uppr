60166643eff26ef444a8c58a29d51a169e9040ed
dmytro <dmytro@ofload.com>
2023-02-06 16:20:08 +0100

    Added article StupidEmailsIntroductions

--- components/articles/index.js
+++ components/articles/index.js
@@ -34,6 +34,7 @@ export {default as KissEmails} from './kiss-emails';
 export {default as CaptainObviousIsWriting} from './captain-obvious-is-writing';
 export {default as ToMeetOrNotToMeet} from './to-meet-or-not-to-meet';
 export {default as ContextIsTheKing} from './context-is-the-king';
+export {default as StupidEmailsIntroductions} from './stupid-emails-introductions';
 
 
 export {default as PageNotFound} from '../common/404/404';
