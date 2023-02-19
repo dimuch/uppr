ccac6850ac6b26f39d76556c7c688b2790aea2a8
dmytro <dmytro@ofload.com>
2023-02-19 20:29:21 +0100

    Fixes for serving resized images

--- components/common/loader/loader.js
+++ components/common/loader/loader.js
@@ -2,7 +2,9 @@ const sizes = [320, 420, 640, 750, 828, 1080, 1200, 1920];
 
 export default function loader({ src, width, quality }){
     const srcParts = src.split('/');
-    let widthIndexToApply = sizes.findIndex(item => item > width);
+    let widthIndexToApply = sizes.findIndex(item => {
+        return item >= width
+    });
 
     if(widthIndexToApply === -1) {
         widthIndexToApply = sizes.length - 1;
