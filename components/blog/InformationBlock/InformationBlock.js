61b5de57f8f41ca9f7727d20bfd1eb9bf5f367a5
dmytro <dmytro@ofload.com>
2023-04-29 20:49:55 +0200

    Added social links icons

--- components/blog/InformationBlock/InformationBlock.js
+++ components/blog/InformationBlock/InformationBlock.js
@@ -1,46 +1,90 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import {Grid, Typography} from '@mui/material';
-import TagIcon from '@mui/icons-material/Tag';
 
 import styles from './styles.module.scss';
 import Link from 'next/link';
+import InstagramIcon from '../../common/icons/instagram-icon';
+import FacebookIcon from '../../common/icons/facebook-icon';
+import TelegramIcon from '../../common/icons/telegram-svgrepo-com';
+import LinkedInIcon from '../../common/icons/linkedin-icon';
 
 export default function InformationBlock({tags}) {
-    return (
-        <Grid
-            item
-            md={4}
-        >
-            <Grid container={true} alignItems="center"
-                  className={styles.upprSectionTitle}
-            >
-                <Grid item md={12} xs={12}>
-                    <Typography variant={'h5'}>Tags</Typography>
-                </Grid>
-            </Grid>
-            <Grid
-                container={true}
-                justifyContent={'flex-start'}
-                alignItems={'center'}
-                className={'uppr-section-content ' + styles.upprSectionContent}
-            >
-                {
-                    tags.map(item => {
-                        return (
-                            <Grid item key={item.name}
-                                  className={styles.tagItem}
-                            >
-                                <Link href={`/blog/post-tag?selectedTag=${item.name}`}>
-                                    <Typography variant={'body2'}>
-                                        {item.name}
-                                    </Typography>
-                                </Link>
-                            </Grid>
-                        )
-                    })
-                }
-            </Grid>
+  return (
+    <Grid
+      item
+      md={4}
+    >
+      <Grid container alignItems="center"
+            className={styles.upprSectionTitle}
+      >
+        <Grid item md={12} xs={12}>
+          <Typography variant={'h5'}>Tags</Typography>
         </Grid>
-    )
+      </Grid>
+      <Grid
+        container
+        justifyContent={'flex-start'}
+        alignItems={'center'}
+        className={'uppr-section-content ' + styles.upprSectionContent}
+      >
+        {
+          tags.map(item => {
+            return (
+              <Grid item key={item.name}
+                    className={styles.tagItem}
+              >
+                <Link href={`/blog/post-tag?selectedTag=${item.name}`}>
+                  <Typography variant={'body2'}>
+                    {item.name}
+                  </Typography>
+                </Link>
+              </Grid>
+            )
+          })
+        }
+      </Grid>
+
+      <Grid container alignItems="center"
+            className={styles.upprSectionTitle}
+      >
+        <Grid item md={12} xs={12}>
+          <Typography variant={'h5'}>Follow Me</Typography>
+        </Grid>
+      </Grid>
+      <div
+        className={'uppr-section-content ' + styles.socialSectionContent}
+      >
+        <div className={styles.socialLink}>
+          <a href="https://www.instagram.com/ivanna.tabachuk"
+             target="_blank" rel="noreferrer"
+          >
+            <InstagramIcon/>
+          </a>
+        </div>
+        <div item xs={3} className={styles.socialLink}>
+          <a href="https://www.facebook.com/ivanna.tabachuk"
+             target="_blank" rel="noreferrer"
+          >
+            <FacebookIcon/>
+          </a>
+        </div>
+        <div item xs={3} className={styles.socialLink}>
+          <a href="https://www.linkedin.com/in/ivannatabachuk"
+             target="_blank" rel="noreferrer"
+          >
+            <LinkedInIcon/>
+          </a>
+        </div>
+        <div item xs={3} className={styles.socialLink}>
+          <a href="https://t.me/emailingskills"
+             target="_blank" rel="noreferrer"
+          >
+            <TelegramIcon/>
+          </a>
+        </div>
+      </div>
+
+    </Grid>
+  )
 }
--- components/common/icons/index.js
+++ components/common/icons/index.js
@@ -5,3 +5,6 @@ export { default as TopFeaturedArticlesIcon } from './top-featured-articles-icon
 export { default as SearchIcon } from './search-icon';
 export { default as LoaderIcon } from './loader-icon';
 export { default as MenuIcon } from './menu-icon';
+export { default as InstagramIcon } from './instagram-icon';
+export { default as FacebookIcon } from './facebook-icon';
+export { default as LinkedInIcon } from './linkedin-icon';
