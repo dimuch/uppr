9b8e8e2224f51b5eac797d93acb507337cffed23
dmytro <dmytro@ofload.com>
2023-05-28 15:36:46 +0200

    Added instagram block to the article footer

--- components/blog/ArticleFooter/ArticleFooter.js
+++ components/blog/ArticleFooter/ArticleFooter.js
@@ -3,6 +3,7 @@ import styles from '../../articles/commonArticleStyles.module.scss';
 import Author from '../Author/Author';
 import Slider from '../Slider/Slider';
 import OthersArticles from '../OthersArticles/OthersArticles';
+import InstagramImages from '../InstagramImages/InstagramImages';
 
 export default function ArticleFooter({articleData}) {
     let width;
@@ -14,6 +15,7 @@ export default function ArticleFooter({articleData}) {
         return null;
     }
     return (
+      <>
         <div className={`${styles.footerContainer}`}>
             <div className={styles.authorBlock}>
                 <Author data={articleData}/>
@@ -36,5 +38,7 @@ export default function ArticleFooter({articleData}) {
                 )
             }
         </div>
+        <InstagramImages />
+      </>
     )
 }
