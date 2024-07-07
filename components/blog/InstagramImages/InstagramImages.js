4d27cf5e2221e56ef89b7a8507ff93f08adb0276
dmytro <dmytro@ofload.com>
2024-07-07 02:58:43 +0200

    Fixed caption comparing

--- components/blog/InstagramImages/InstagramImages.js
+++ components/blog/InstagramImages/InstagramImages.js
@@ -1,18 +1,17 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
 import InstagramIcon from '../../common/icons/instagram-icon';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 
-export default function InstagramImages({instaItems, sectionTitle}) {
+export default function InstagramImages({ instaItems, sectionTitle }) {
   const wrapperRef = useRef();
   const [scrollTop, setScrollTop] = useState(0);
   const [top, setTop] = useState(0);
   const [screenHeight, setScreenHeight] = useState(0);
 
-
   useEffect(() => {
-    const handleScroll = (event) => {
-      const {top} = wrapperRef?.current?.getBoundingClientRect() || {top: 0};
+    const handleScroll = event => {
+      const { top } = wrapperRef?.current?.getBoundingClientRect() || { top: 0 };
       const currentScreenHeight = window.innerHeight;
 
       setScreenHeight(currentScreenHeight);
@@ -27,54 +26,37 @@ export default function InstagramImages({instaItems, sectionTitle}) {
     };
   }, []);
 
-  const wrapperAnimateClass = screenHeight - top > 200 ?
-    `${styles.wrapper} ${styles.wrapperOpacityIn}` : `${styles.wrapper}`;
+  const wrapperAnimateClass =
+    screenHeight - top > 200 ? `${styles.wrapper} ${styles.wrapperOpacityIn}` : `${styles.wrapper}`;
 
   return (
     <div className={styles.section}>
       <p className={styles.sectionTitle}>{sectionTitle}</p>
-      <div className={styles.sectionTitleSplitter}/>
+      <div className={styles.sectionTitleSplitter} />
       <p className={styles.shadowTitle}>Follow</p>
 
       <div className={wrapperAnimateClass} ref={wrapperRef}>
-        {
-          instaItems?.length > 0 && instaItems.map((item, index) => {
+        {instaItems?.length > 0 &&
+          instaItems.map((item, index) => {
             if (!item?.src) {
               return (
-                <a
-                  key={item.alt}
-                  href={item.link}
-                  target="_blank" rel="noreferrer"
-                  className={styles.centerImage}
-                >
-                  <InstagramIcon/>
-                  <Typography
-                    className={styles.instaCaption}
-                    variant={'h5'}
-                  >
+                <a key={item.alt} href={item.link} target="_blank" rel="noreferrer" className={styles.centerImage}>
+                  <InstagramIcon />
+                  <Typography className={styles.instaCaption} variant={'h5'}>
                     {item.alt}
                   </Typography>
                 </a>
-              )
+              );
             }
 
             return (
-              <a
-                key={item.alt + index}
-                href={item.link}
-                target="_blank" rel="noreferrer"
-                className={styles.leftFirst}>
-                <img
-                  src={item.src}
-                  width={item.width}
-                  height={item.height}
-                  alt={item.alt}
-                />
+              <a key={item.alt + index} href={item.link} target="_blank" rel="noreferrer" className={styles.leftFirst}>
+                <img src={item.src} width={item.width} height={item.height} alt={item.alt} />
                 <Typography
                   className={styles.instaCaption}
                   variant={'h5'}
                   sx={{
-                    color:'black',
+                    color: 'black',
                     position: 'absolute',
                     top: 'calc(100% + 16px)',
                     bottom: '-2rem',
@@ -84,11 +66,9 @@ export default function InstagramImages({instaItems, sectionTitle}) {
                   {item?.title}
                 </Typography>
               </a>
-            )
-
-          })
-        }
+            );
+          })}
       </div>
     </div>
-  )
+  );
 }
